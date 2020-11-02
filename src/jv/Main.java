0c6ecc95ccf26cf02751a2cc8fbeb070a23cc258
Gilles <chekunive@gmail.com>
2020-11-02 09:24:26 +0100

    init

--- src/jv/Main.java
+++ src/jv/Main.java
@@ -7,6 +7,17 @@ public class Main {
         Board b = new Board();
         Engine e = new Engine();
         e.legalmoves(b);
+        // python
+//        1 20
+//        2	400
+//        3	8902
+//        4	197281
+//        5	4865609
+        // java
+//        depth 2:400
+//        depth 3:8902
+//        depth 4:197281
+//        depth 5:4865609
         e.perft(4, b);
     }
 }
